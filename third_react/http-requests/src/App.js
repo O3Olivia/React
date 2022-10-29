85d39cab5b551255d584deb0fea27b86b4aa3804
O3Olivia <cherishtmwu@gmail.com>
2022-10-29 21:33:22 +0900

    add loading

--- third_react/http-requests/src/App.js
+++ third_react/http-requests/src/App.js
@@ -5,8 +5,10 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   async function fetchMovieHandler() {
+    setIsLoading(true); // Loading중임
     const response = await fetch("https://swapi.dev/api/films");
     const data = await response.json(); // json() 변환과정
 
@@ -22,6 +24,7 @@ function App() {
     // map() => 넘겨받은 배열의 모든 객체를 새로운 객체로 변환시킴.
     // movieData라고 이름을 붙인 데이터를 가져오는데, 이 중 3가지만 가져오고 싶기 때문에 3가지의 이름을 설정
     setMovies(transformedMovies);
+    setIsLoading(false); // loading이 끝났으므로
   }
   // data에서 results 부분을 가져와서 state에 저장시킬 것임.
   // fetch API를 사용함 ('요청하는 URL을 https://를 붙여서 적는다', {자바스크립트 객체 전달 ->get() 메소드를 사용하기 때문에 지금은 필요없음 })
@@ -33,7 +36,9 @@ function App() {
         {/* Fetch Movies button을 누르면 영화정보를 가져옴 */}
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>Found No Movies</p>}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
