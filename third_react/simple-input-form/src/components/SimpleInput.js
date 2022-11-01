76c3fe41d639363fc57f5a4a52cbe33f26926afa
O3Olivia <cherishtmwu@gmail.com>
2022-11-02 02:49:09 +0900

    refactoring

--- third_react/simple-input-form/src/components/SimpleInput.js
+++ third_react/simple-input-form/src/components/SimpleInput.js
@@ -1,16 +1,11 @@
-import { useEffect, useRef, useState } from "react";
+import { useState } from "react";
 
 const SimpleInput = (props) => {
-  const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState("");
-  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
 
-  useEffect(() => {
-    if (enteredNameIsValid) {
-      console.log("Name Input is Valid");
-    }
-  }, [enteredNameIsValid]);
+  const enteredNameIsValid = enteredName.trim() !== "";
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
 
   const nameInputChangeHandler = (e) => {
     setEnteredName(e.target.value);
@@ -18,32 +13,19 @@ const SimpleInput = (props) => {
 
   const nameInputBlurHandler = (e) => {
     setEnteredNameTouched(true); // input값에서 focus 잃었다는 의미는 '이미 input 공간을 touched 했다는 의미'
-
-    if (enteredName.trim() === "") {
-      setEnteredNameIsValid(false);
-      return; // 아래 실행되지 않도록 함
-    }
   };
 
   const formSubHandler = (e) => {
     e.preventDefault();
 
     setEnteredNameTouched(true); // form이 제출되는 순간 모든 입력값이 사용자가 확인했다고 생각하기 때문에 true
-    if (enteredName.trim() === "") {
-      setEnteredNameIsValid(false);
-      return; // 아래 실행되지 않도록 함
+    if (!enteredNameIsValid) {
+      return;
     }
 
-    setEnteredNameIsValid(true);
-    console.log(enteredName);
-
-    const enteredValue = nameInputRef.current.value;
-    console.log(enteredValue);
-
     setEnteredName("");
+    setEnteredNameTouched(false);
   };
-
-  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
 
   const nameInputClasses = nameInputIsInvalid
     ? "form-control  invalid"
@@ -54,7 +36,6 @@ const SimpleInput = (props) => {
       <div className={nameInputClasses}>
         <label htmlFor="name">Your Name</label>
         <input
-          ref={nameInputRef}
           type="text"
           id="name"
           onChange={nameInputChangeHandler}
