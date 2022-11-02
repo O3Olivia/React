c3ebb42c9e633e3fabcf9d2858fb06f53c87af95
O3Olivia <cherishtmwu@gmail.com>
2022-11-02 18:29:22 +0900

    finish BasicForm

--- third_react/simple-input-form/src/components/BasicForm.js
+++ third_react/simple-input-form/src/components/BasicForm.js
@@ -10,8 +10,48 @@ const BasicForm = (props) => {
     reset: resetFirstNameInput,
   } = useInput((value) => value.trim() !== "");
 
+  const {
+    value: enteredLastName,
+    isValid: enteredLastNameIsValid,
+    hasError: lastNameInputHasError,
+    valueChangeHandler: lastNameChangedHandler,
+    inputBlurHandler: lastNameBlurHandler,
+    reset: resetLastNameInput,
+  } = useInput((value) => value.trim() !== "");
+
+  const {
+    value: enteredEmail,
+    isValid: enteredEmailIsValid,
+    hasError: emailInputHasError,
+    valueChangeHandler: emailChangedHandler,
+    inputBlurHandler: emailBlurHandler,
+    reset: resetEmailNameInput,
+  } = useInput((value) => value.includes("@"));
+
+  let formIsValid = false;
+
+  if (
+    enteredFirstNameIsValid &&
+    enteredLastNameIsValid &&
+    enteredEmailIsValid
+  ) {
+    formIsValid = true;
+  }
+
+  const formSubHandler = (e) => {
+    e.preventDefault();
+
+    if (!enteredFirstName) {
+      return;
+    }
+    resetFirstNameInput();
+    resetLastNameInput();
+    resetEmailNameInput();
+    console.log("submit");
+  };
+
   return (
-    <form>
+    <form onSubmit={formSubHandler}>
       <div className="control-group">
         <div className="form-control">
           <label htmlFor="name">First Name</label>
@@ -26,17 +66,37 @@ const BasicForm = (props) => {
             <p className="error-text">First Name must not be empty.</p>
           )}
         </div>
+
         <div className="form-control">
           <label htmlFor="name">Last Name</label>
-          <input type="text" id="name" />
+          <input
+            type="text"
+            id="name"
+            onChange={lastNameChangedHandler}
+            onBlur={lastNameBlurHandler}
+            value={enteredLastName}
+          />
+          {lastNameInputHasError && (
+            <p className="error-text">Last Name must not be empty.</p>
+          )}
         </div>
       </div>
+
       <div className="form-control">
-        <label htmlFor="name">E-Mail Address</label>
-        <input type="text" id="name" />
+        <label htmlFor="email">E-Mail Address</label>
+        <input
+          type="email"
+          id="email"
+          onChange={emailChangedHandler}
+          onBlur={emailBlurHandler}
+          value={enteredEmail}
+        />
+        {emailInputHasError && (
+          <p className="error-text">Email must not be empty.</p>
+        )}
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
