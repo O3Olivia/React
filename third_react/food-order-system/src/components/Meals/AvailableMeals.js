86a7b5369fbad776c2aec87c52284134048d86e7
O3Olivia <cherishtmwu@gmail.com>
2022-11-03 03:03:06 +0900

    http request GET meals

--- third_react/food-order-system/src/components/Meals/AvailableMeals.js
+++ third_react/food-order-system/src/components/Meals/AvailableMeals.js
@@ -1,38 +1,35 @@
-import Card from "../UI/Card";
+import { useEffect, useState } from "react";
 
+import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
 import classes from "./AvailableMeals.module.css";
 
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Potato Pizza",
-    description: "Classic and Homemade style potato pizza",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Fried Chicken",
-    description:
-      "The crispiest, most flavorful crunchy outside with moist and juicy meat on the inside.",
-    price: 26.5,
-  },
-  {
-    id: "m3",
-    name: "Chocolate Cake",
-    description: "Super moist crumb and fudgy",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Korean BBQ",
-    description: "Korean style BBQ",
-    price: 35.99,
-  },
-];
-
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const [meals, setMeals] = useState([]);
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch(
+        "https://react-http-2a48b-default-rtdb.asia-southeast1.firebasedatabase.app/meals.json"
+      );
+      const responseData = await response.json();
+
+      const loadedMeals = [];
+
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
+      }
+
+      setMeals(loadedMeals);
+    };
+    fetchMeals();
+  }, []);
+
+  const mealsList = meals.map((meal) => (
     <MealItem
       id={meal.id}
       key={meal.id}
