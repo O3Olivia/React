1fd2c82c9c77ae9351ddf7428352884cbcdd4984
O3Olivia <cherishtmwu@gmail.com>
2022-11-05 19:56:51 +0900

    add validation

--- third_react/food-order-system/src/components/Cart/Checkout.js
+++ third_react/food-order-system/src/components/Cart/Checkout.js
@@ -1,8 +1,18 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 
 import classes from "./Checkout.module.css";
 
+const isEmpty = (value) => value.trim() === "";
+const isFiveChars = (value) => value.trim().length === 5;
+
 const Checkout = (props) => {
+  const [formInputsValidity, setFormInputsValidity] = useState({
+    name: true,
+    street: true,
+    city: true,
+    postalCode: true,
+  });
+
   const nameInputRef = useRef();
   const streetInputRef = useRef();
   const cityInputRef = useRef();
@@ -15,29 +25,77 @@ const Checkout = (props) => {
     const enteredStreet = streetInputRef.current.value;
     const enteredCity = cityInputRef.current.value;
     const enteredPostalCode = postalCodeInputRef.current.value;
+
+    const enteredNameIsValid = !isEmpty(enteredName);
+    const enteredStreetIsValid = !isEmpty(enteredStreet);
+    const enteredCityIsValid = !isEmpty(enteredCity);
+    const enteredPostalCodeIsValid = isFiveChars(enteredPostalCode);
+
+    setFormInputsValidity({
+      name: enteredCityIsValid,
+      street: enteredStreetIsValid,
+      city: enteredCityIsValid,
+      postalCode: enteredPostalCodeIsValid,
+    });
+
+    const formIsValid =
+      enteredNameIsValid &&
+      enteredStreetIsValid &&
+      enteredCityIsValid &&
+      enteredPostalCodeIsValid;
+
+    if (!formIsValid) {
+      return;
+    }
+
+    // submit cart data
   };
+
+  const nameControlClasses = `${classes.control} ${
+    formInputsValidity.name ? "" : classes.invalid
+  }`;
+
+  const streetControlClasses = `${
+    formInputsValidity.street ? classes.street : classes.invalidStreet
+  }`;
+
+  const cityControlClasses = `${
+    formInputsValidity.city ? classes.city : classes.invalidCity
+  }`;
+
+  const postalCodeControlClasses = `${classes.control} ${
+    formInputsValidity.postalCode ? "" : classes.invalid
+  }`;
 
   return (
     <form className={classes.form} onSubmit={confirmHandler}>
-      <div className={classes.control}>
+      <div className={nameControlClasses}>
         <label httpFor="name">Your Name</label>
         <input type="text" id="name" ref={nameInputRef} />
+        {!formInputsValidity.name && <p>Please enter a valid name</p>}
       </div>
 
       <div className={classes.control}>
-        <div className={classes.street}>
+        <div className={streetControlClasses}>
           <label httpFor="street">Street</label>
           <input type="text" id="street" ref={streetInputRef} />
+          {!formInputsValidity.street && (
+            <p>Please enter a valid street name</p>
+          )}
         </div>
-        <div className={classes.city}>
+        <div className={cityControlClasses}>
           <label httpFor="city">City</label>
           <input type="text" id="city" ref={cityInputRef} />
+          {!formInputsValidity.city && <p>Please enter a valid city</p>}
         </div>
       </div>
 
-      <div className={classes.control}>
+      <div className={postalCodeControlClasses}>
         <label httpFor="postal">Postal Code</label>
         <input type="text" id="postal" ref={postalCodeInputRef} />
+        {!formInputsValidity.postalCode && (
+          <p>Please enter a valid postal code</p>
+        )}
       </div>
       {/* Postal은 숫자지만 text로 처리해야 NULL값으로 처리되지 않고 0으로 시작할 수 있다. */}
 
