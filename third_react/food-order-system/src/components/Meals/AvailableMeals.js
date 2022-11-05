f697d94e4159ead0799f9535466d20cb62d78914
O3Olivia <cherishtmwu@gmail.com>
2022-11-05 17:02:35 +0900

    add error message

--- third_react/food-order-system/src/components/Meals/AvailableMeals.js
+++ third_react/food-order-system/src/components/Meals/AvailableMeals.js
@@ -7,12 +7,17 @@ import classes from "./AvailableMeals.module.css";
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
 
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(
         "https://react-http-2a48b-default-rtdb.asia-southeast1.firebasedatabase.app/meals.json"
       );
+
+      if (!response.ok) {
+        throw new Error("Something Went Wrong!");
+      }
       const responseData = await response.json();
 
       const loadedMeals = [];
@@ -29,13 +34,25 @@ const AvailableMeals = () => {
       setMeals(loadedMeals);
       setIsLoading(false); // loading 끝
     };
-    fetchMeals();
+
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+      // throw new Error("Something Went Wrong!"); 나옴
+    });
   }, []);
 
   if (isLoading) {
     return (
       <section className={classes.MealsLoading}>
         <p>Loading...</p>
+      </section>
+    );
+  }
+  if (httpError) {
+    return (
+      <section className={classes.MealsError}>
+        <p>{httpError}</p>
       </section>
     );
   }
