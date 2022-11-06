1b57157c134ffebe37485c88bb2daf5280a37f4a
O3Olivia <cherishtmwu@gmail.com>
2022-11-07 01:32:15 +0900

    using react redux in components

--- third_react/redux-counter/src/components/Counter.js
+++ third_react/redux-counter/src/components/Counter.js
@@ -1,12 +1,14 @@
-import classes from './Counter.module.css';
+import { useSelector } from "react-redux";
+import classes from "./Counter.module.css";
 
 const Counter = () => {
+  const counter = useSelector((state) => state.counter); // fn을 주면, react-redux가 redux의 state를 보낸다. fn가 어떤 데이터를 store에서 추출할지 결정. =>  redux가 관리하는 가장 최신의 counter를 받을 수 있다.
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
