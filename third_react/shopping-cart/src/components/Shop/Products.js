64c23718c798d218b5b3692f19130b90744da772
O3Olivia <cherishtmwu@gmail.com>
2022-11-07 20:01:22 +0900

    redux toggle button

--- third_react/shopping-cart/src/components/Shop/Products.js
+++ third_react/shopping-cart/src/components/Shop/Products.js
@@ -1,16 +1,35 @@
 import ProductItem from "./ProductItem";
 import classes from "./Products.module.css";
 
+const DUMMY_PRODUCT = [
+  {
+    id: "p1",
+    price: 6,
+    title: "My First Book",
+    describe: "The First Book I ever Wrote",
+  },
+  {
+    id: "p2",
+    price: 8,
+    title: "My Second Book",
+    describe: "The Second Book I ever Wrote",
+  },
+];
+
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title="Test"
-          price={6}
-          description="This is a first product - amazing!"
-        />
+        {DUMMY_PRODUCT.map((product) => (
+          <ProductItem
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.describe}
+          />
+        ))}
       </ul>
     </section>
   );
