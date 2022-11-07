036ab0243049b3389965183554be619281ac7703
O3Olivia <cherishtmwu@gmail.com>
2022-11-07 20:23:23 +0900

    redux toggle button

--- third_react/shopping-cart/src/store/cart-slice.js
+++ third_react/shopping-cart/src/store/cart-slice.js
@@ -11,9 +11,11 @@ const cartSlice = createSlice({
     addItemToCart(state, action) {
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
+      state.totalQuantity++;
+
       if (!existingItem) {
         state.items.push({
-          itemId: newItem.id,
+          id: newItem.id,
           price: newItem.price,
           quantity: 1,
           totalPrice: newItem.price,
@@ -27,6 +29,8 @@ const cartSlice = createSlice({
     removeItemFromCart(state, action) {
       const id = action.payload;
       const existingItem = state.items.find((item) => item.id === id);
+      state.totalQuantity--;
+
       if (existingItem.quantity === 1) {
         state.items = state.items.filter((item) => item.id !== id); // id와 동일하지 않은 id만 필터링해서 제거하고 새로운 array
       } else {
