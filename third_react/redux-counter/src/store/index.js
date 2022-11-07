64aa91be6ed071467b7d23f5a96655e547a4b08a
O3Olivia <cherishtmwu@gmail.com>
2022-11-07 17:32:32 +0900

    split slices

--- third_react/redux-counter/src/store/index.js
+++ third_react/redux-counter/src/store/index.js
@@ -1,79 +1,9 @@
-import { createSlice, configureStore } from "@reduxjs/toolkit";
-
-const initialCounterState = { counter: 0, showCounter: true };
-
-const counterSlice = createSlice({
-  name: "counter",
-  initialState: initialCounterState,
-  reducers: {
-    increment(state) {
-      state.counter++;
-    },
-    decrement(state) {
-      state.counter--;
-    },
-    increase(state, action) {
-      state.counter = state.counter + action.payload;
-    },
-    toggleCounter(state) {
-      state.showCounter = !state.showCounter;
-    },
-  },
-});
-
-// const counterReducer = (state = initialState, action) => {
-//   if (action.type === "increment") {
-//     return {
-//       counter: state.counter + 1,
-//       showCounter: state.showCounter,
-//     };
-//     // state.counter++; return state -> 이렇게 하면 절대 안된다. redux를 사용할땐 state를 직접적으로 변경시킬 수 없다.
-//     // 대신, 새로운 state 객체를 return하여 재정의 시켜야한다.
-//     // 그렇기 때문에 return {counter:} 형식으로 사용해야한다.
-//   }
-//   if (action.type === "increase") {
-//     return {
-//       counter: state.counter + action.value,
-//       showCounter: state.showCounter,
-//     };
-//   }
-//   if (action.type === "decrement") {
-//     return {
-//       counter: state.counter - 1,
-//       showCounter: state.showCounter,
-//     };
-//   }
-//   if (action.type === "toggle") {
-//     return {
-//       counter: state.counter,
-//       showCounter: !state.showCounter,
-//     };
-//   }
-//   return state;
-// };
-
-const initialAuthState = {
-  isAuthenticated: false,
-};
-
-const authSlice = createSlice({
-  name: "authentication",
-  initialState: initialAuthState,
-  reducers: {
-    login(state) {
-      state.isAuthenticated = true;
-    },
-    logout(state) {
-      state.isAuthenticated = false;
-    },
-  },
-});
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "./counter";
+import authReducer from "./auth";
 
 const store = configureStore({
-  reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
+  reducer: { counter: counterReducer, auth: authReducer },
 });
-
-export const counterActions = counterSlice.actions;
-export const authActions = authSlice.actions;
 
 export default store;
