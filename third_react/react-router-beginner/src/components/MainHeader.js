e217ac4fbdea7e6f04a6c3eecaf1aee793279dad
O3Olivia <cherishtmwu@gmail.com>
2022-11-09 05:38:21 +0900

    add navLink

--- third_react/react-router-beginner/src/components/MainHeader.js
+++ third_react/react-router-beginner/src/components/MainHeader.js
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 
 import classes from "./MainHeader.module.css";
 
@@ -8,11 +8,19 @@ const MainHeader = () => {
       <nav>
         <ul>
           <li>
-            <Link to="/welcome">Welcome</Link>
+            <Link to="/">Home</Link>
           </li>
           <li>
-            <Link to="/products">Products</Link>
+            <NavLink activeClassName={classes.active} to="/welcome">
+              Welcome
+            </NavLink>
           </li>
+          <li>
+            <NavLink activeClassName={classes.active} to="/products">
+              Products
+            </NavLink>
+          </li>
+          {/* link와 navLink의 차이: a태그가 active 되는지 안되는지 여부. navLink는 active가 되어서 현재 그 페이지에 들어와있다는 css 설정 가능하게 해준다(:active기능) */}
         </ul>
       </nav>
     </header>
