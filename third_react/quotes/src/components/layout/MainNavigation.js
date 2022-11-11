d862bd1028a2a124a6683a9ca16c5afaefe022c7
O3Olivia <cherishtmwu@gmail.com>
2022-11-11 17:32:51 +0900

    add layout

---
+++ third_react/quotes/src/components/layout/MainNavigation.js
@@ -0,0 +1,27 @@
+import { NavLink } from "react-router-dom";
+
+import classes from "./MainNavigation.module.css";
+
+const MainNavigation = () => {
+  return (
+    <header className={classes.header}>
+      <div className={classes.logo}>ALL QUOTES</div>
+      <nav className={classes.nav}>
+        <ul>
+          <li>
+            <NavLink to="/quotes" activeClassName={classes.active}>
+              ALL Quotes
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/new-quote" activeClassName={classes.active}>
+              Add Quotes
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default MainNavigation;
