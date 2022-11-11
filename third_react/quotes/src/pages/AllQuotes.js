7cefbd5bade9cb14b7afd0f8ce926718cbd7d97a
O3Olivia <cherishtmwu@gmail.com>
2022-11-11 17:43:20 +0900

    add some dummy_data

--- third_react/quotes/src/pages/AllQuotes.js
+++ third_react/quotes/src/pages/AllQuotes.js
@@ -1,5 +1,12 @@
+import QuoteList from "../components/quotes/QuoteList";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Olivia", text: "Learning React is Super Fun!!" },
+  { id: "q2", author: "Rachel", text: "Learning React is Great" },
+];
+
 const AllQuotes = () => {
-  return <h1>All Quotes Page!</h1>;
+  return <QuoteList quotes={DUMMY_QUOTES} />;
 };
 
 export default AllQuotes;
