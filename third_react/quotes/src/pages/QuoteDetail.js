002ded5d3100211fe6e413eaebe59294b771c3fb
O3Olivia <cherishtmwu@gmail.com>
2022-11-11 17:51:39 +0900

    edit detail page

--- third_react/quotes/src/pages/QuoteDetail.js
+++ third_react/quotes/src/pages/QuoteDetail.js
@@ -2,14 +2,27 @@ import { Fragment } from "react";
 import { useParams, Route } from "react-router-dom";
 
 import Comments from "../components/comments/Comments";
+import HighLightedQuote from "../components/quotes/HighlightedQuote";
+import QuoteList from "../components/quotes/QuoteList";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Olivia", text: "Learning React is Super Fun!!" },
+  { id: "q2", author: "Rachel", text: "Learning React is Great" },
+];
 
 const QuoteDetail = () => {
-  const param = useParams();
+  const params = useParams();
+
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId); //params = URL
+
+  if (!quote) {
+    return <p>No quote found!</p>;
+  }
+
   return (
     <Fragment>
-      <h1>Quotes Detail Page!</h1>
-      <p>{param.quoteId}</p>
-      <Route path={`/quotes/${param.quoteId}/comments`}>
+      <HighLightedQuote text={quote.text} author={quote.author} />
+      <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
       {/* <Route path="/quotes/:quoteId/comments" /> */}
