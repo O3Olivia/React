097aaa555c30e29986453ec349e7cb8c90201876
O3Olivia <cherishtmwu@gmail.com>
2022-11-14 19:45:47 +0900

    http request

--- third_react/quotes/src/pages/NewQuote.js
+++ third_react/quotes/src/pages/NewQuote.js
@@ -1,22 +1,32 @@
+import { useEffect } from "react";
 import { useHistory } from "react-router-dom";
 // useHistory: 브라우저의 history를 변경할 수 있다
 
 import QuoteForm from "../components/quotes/QuoteForm";
+import useHttp from "../hooks/use-http";
+import { addQuote } from "../lib/api";
 
 const NewQuote = () => {
+  const { sendRequest, status } = useHttp(addQuote);
   const history = useHistory();
 
-  const addQuoteHandler = (quoteData) => {
-    console.log(quoteData);
+  useEffect(() => {
+    if (status === "completed") {
+      history.push("/quotes");
+    }
+  }, [status, history]);
 
-    history.push("/quotes");
+  const addQuoteHandler = (quoteData) => {
+    sendRequest(quoteData);
     // push = back button누르면 이전 페이지로 돌아갈 수 있다. (like add new page)
     // replace = back button 눌러도 이전 페이지로 돌아갈 수 없다. (like redirection)
 
     // add페이지에서 add quote 버튼 누르면 바로 다른 페이지로 이동하려고
   };
 
-  return <QuoteForm onAddQuote={addQuoteHandler} />;
+  return (
+    <QuoteForm isLoading={status === "pending"} onAddQuote={addQuoteHandler} />
+  );
 };
 
 export default NewQuote;
--- third_react/quotes/src/pages/QuoteDetail.js
+++ third_react/quotes/src/pages/QuoteDetail.js
@@ -1,28 +1,47 @@
-import { Fragment } from "react";
+import { Fragment, useEffect } from "react";
 import { useParams, Route, Link, useRouteMatch } from "react-router-dom";
 
 import Comments from "../components/comments/Comments";
 import HighLightedQuote from "../components/quotes/HighlightedQuote";
-import QuoteList from "../components/quotes/QuoteList";
-
-const DUMMY_QUOTES = [
-  { id: "q1", author: "Olivia", text: "Learning React is Super Fun!!" },
-  { id: "q2", author: "Rachel", text: "Learning React is Great" },
-];
+import useHttp from "../hooks/use-http";
+import { getSingleQuote } from "../lib/api";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
 
 const QuoteDetail = () => {
   const match = useRouteMatch();
   const params = useParams();
+  const { quoteId } = params;
 
-  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId); //params = URL
+  const {
+    sendRequest,
+    status,
+    data: loadedQuote,
+    error,
+  } = useHttp(getSingleQuote, true);
 
-  if (!quote) {
-    return <p>No quote found!</p>;
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
+
+  if (status === "pending") {
+    return (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <p className="centered">{error}</p>;
+  }
+
+  if (!loadedQuote.text) {
+    return <p className="centered">No quote found!</p>;
   }
 
   return (
     <Fragment>
-      <HighLightedQuote text={quote.text} author={quote.author} />
+      <HighLightedQuote text={loadedQuote.text} author={loadedQuote.author} />
       <Route path={`${match.path}`} exact>
         {/* exactly /comment 없으면 Load Comments가 보이고, 이걸로 Path가 정확하지 않으면 LoadComments가 안뜬다*/}
         <div className="centered">
