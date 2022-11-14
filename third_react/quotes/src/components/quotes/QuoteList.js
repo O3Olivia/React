28d4bc7039b08db4c4a7ecb6ce1c52f0f2674750
O3Olivia <cherishtmwu@gmail.com>
2022-11-14 18:13:58 +0900

    sort, asc, desc

--- third_react/quotes/src/components/quotes/QuoteList.js
+++ third_react/quotes/src/components/quotes/QuoteList.js
@@ -1,18 +1,45 @@
 import { Fragment } from "react";
-import { useHistory } from "react-router-dom";
+import { useHistory, useLocation } from "react-router-dom";
 
 import QuoteItem from "./QuoteItem";
 import classes from "./QuoteList.module.css";
 
+const sortQuotes = (quotes, ascending) => {
+  return quotes.sort((quoteA, quoteB) => {
+    if (ascending) {
+      return quoteA.id > quoteB.id ? 1 : -1;
+    } else {
+      return quoteA.id < quoteB.id ? 1 : -1;
+    }
+  });
+};
+
 const QuoteList = (props) => {
-  const changeSortingHandler = () => {};
+  const history = useHistory();
+  const location = useLocation();
+
+  console.log(history);
+  console.log(location);
+
+  const queryParams = new URLSearchParams(location.search);
+  // 브라우저에 원래 있는 함수로 React router나 react에서 오지 않는다. 자바스크립트의 기본 생성자 함수 겸 기본 class인데 브라우저에서 사용할 수 있다.
+
+  const isSortingAscending = queryParams.get("sort") === "asc"; // sort는 search에서 key 이름임 key의 value가 asc와 같으면 true, 틀리면 false
+
+  const sortedQuotes = sortQuotes(props.quotes, isSortingAscending);
+
+  const changeSortingHandler = () => {
+    history.push("/quotes?sort=" + (isSortingAscending ? "desc" : "asc"));
+  }; // asc이면 desc로 바꿔주는 것
   return (
     <Fragment>
       <div className={classes.sorting}>
-        <button onClick={changeSortingHandler}>Sort Ascending</button>
+        <button onClick={changeSortingHandler}>
+          Sort {isSortingAscending ? "Descending" : "Ascending"}
+        </button>
       </div>
       <ul className={classes.list}>
-        {props.quotes.map((quote) => (
+        {sortedQuotes.map((quote) => (
           <QuoteItem
             key={quote.id}
             id={quote.id}
