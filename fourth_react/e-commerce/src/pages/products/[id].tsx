ca33e335c72eb4ace4423412ea358b8f753f9c2d
O3Olivia <cherishtmwu@gmail.com>
2022-12-01 23:29:47 +0900

    detail page

--- fourth_react/e-commerce/src/pages/products/[id].tsx
+++ fourth_react/e-commerce/src/pages/products/[id].tsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import { useParams, Link } from 'react-router-dom'
+import { QueryKeys, fetcher } from '../../queryClient'
+import { useQuery } from 'react-query'
+import { Product } from '../../types'
 
 const PrdDetailPage = () => {
+  const { id} = useParams()
+
+  const { data } = useQuery<Product>([QueryKeys.PRODUCTS, id], () => fetcher({
+    method: 'GET',
+    path: `/products/${id}`,
+  }),)
+
+  if (!data) return null;
+
+  const { category, title, image, description, price, rating: { rate} } = data
   return (
-    <div>상세페이지</div>
+    <li className='product-detail'>
+      <p className='product-detail__category'>{ category}</p>
+    
+    </li>
   )
 }
 
