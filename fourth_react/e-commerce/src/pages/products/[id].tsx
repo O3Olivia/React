58f567e8a8dce35a6a2ea867e4dcc55770ef7563
O3Olivia <cherishtmwu@gmail.com>
2022-12-02 02:45:10 +0900

    detail page

--- fourth_react/e-commerce/src/pages/products/[id].tsx
+++ fourth_react/e-commerce/src/pages/products/[id].tsx
@@ -2,6 +2,7 @@ import { useParams, Link } from 'react-router-dom'
 import { QueryKeys, fetcher } from '../../queryClient'
 import { useQuery } from 'react-query'
 import { Product } from '../../types'
+import { TfiHeart } from "react-icons/tfi";
 
 const PrdDetailPage = () => {
   const { id} = useParams()
@@ -16,7 +17,17 @@ const PrdDetailPage = () => {
   const { category, title, image, description, price, rating: { rate} } = data
   return (
     <li className='product-detail'>
-      <p className='product-detail__category'>{ category}</p>
+      <div className='image'>
+        <img className='product-detail__image' src={image} />
+      </div>
+      <div className='title'>
+        <p className='product-detail__category'>{category}</p>
+        <h3 className='product-detail__title'>{title}</h3>
+        <span className="product-detail__price">${price} | </span>
+        <span className="product-detail__rating">
+          <span className="heartEmoji"><TfiHeart /></span>{rate}</span>
+         <p className='product-detail__description'>{description}</p>
+      </div>
     
     </li>
   )
