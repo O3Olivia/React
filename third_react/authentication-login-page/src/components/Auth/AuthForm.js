0c457f22f038f03c67607ec567483e43e30ea1de
O3Olivia <cherishtmwu@gmail.com>
2022-12-02 20:45:33 +0900

    add error alert message

--- third_react/authentication-login-page/src/components/Auth/AuthForm.js
+++ third_react/authentication-login-page/src/components/Auth/AuthForm.js
@@ -6,6 +6,8 @@ const AuthForm = () => {
   const emailInputRef = useRef();
   const pwdInputRef = useRef();
   const [isLogin, setIsLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  // 로딩상태가되면 isLoading이 true가 되도록 설정
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
@@ -18,31 +20,51 @@ const AuthForm = () => {
     const enteredPwd = pwdInputRef.current.value;
 
     // Validation
+    setIsLoading(true); // submit하면 우선 loading이 되도록 true
+    let url;
     if (isLogin) {
+      // signIn URL 가져오기
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyA4bpMeHWiBx73UGNr9rw0xi0iysGp5feo";
     } else {
-      fetch(
-        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyA4bpMeHWiBx73UGNr9rw0xi0iysGp5feo",
-        {
-          method: "POST",
-          body: JSON.stringify({
-            email: enteredEmail,
-            password: enteredPwd,
-            returnSecureToken: true,
-          }),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      ).then((response) => {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyA4bpMeHWiBx73UGNr9rw0xi0iysGp5feo";
+    }
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        email: enteredEmail,
+        password: enteredPwd,
+        returnSecureToken: true,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        setIsLoading(false); // 로그인 되든 안되든 response를 받았으니까 로딩을 멈춘다.
         if (response.ok) {
+          return response.json();
         } else {
           // error
           return response.json().then((data) => {
-            alert(data.error.message);
+            let errorMsg = "Authentication failed";
+            // if (data && data.error && data.error.message) {
+            //   // 이 모든게 참이면
+            //   errorMsg = data.error.message;
+            //   // errorMsg에 좀 더 specific 메세지를 설정하여 세부적인 메세지를 alert에 띄울 수 있다.
+            // }
+
+            throw new Error(errorMsg);
           });
         }
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        alert(error.errorMsg);
       });
-    }
   };
 
   return (
@@ -58,7 +80,10 @@ const AuthForm = () => {
           <input type="password" id="password" required ref={pwdInputRef} />
         </div>
         <div className={classes.actions}>
-          <button>{isLogin ? "Login" : "Create Account"}</button>
+          {!isLoading && (
+            <button>{isLogin ? "Login" : "Create Account"}</button>
+          )}
+          {isLoading && <p>Sending Request...</p>}
           <button
             type="button"
             className={classes.toggle}
--- third_react/to-do-list/src/components/ToDo/ToDoInput/ToDoInput.js
+++ third_react/to-do-list/src/components/ToDo/ToDoInput/ToDoInput.js
@@ -1,29 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import Button from '../../UI/Button/Button';
-import './ToDoInput.css';
+import "./ToDoInput.css";
 
 const ToDoInput = (props) => {
-    const [enteredValue, setEnteredValue] = useState('');
+  const [enteredValue, setEnteredValue] = useState("");
 
-    const toDoInputHandler = (e) => {
-        setEnteredValue(e.target.value);
-    };
+  const toDoInputHandler = (e) => {
+    setEnteredValue(e.target.value);
+  };
 
-    const formSubmitHandler = (e) => {
-        e.preventDefault();
-        props.onAddTodo(enteredValue);
-    };
+  const formSubmitHandler = (e) => {
+    e.preventDefault();
+    props.onAddTodo(enteredValue);
+  };
 
-    return (
-        <form onSubmit={formSubmitHandler}>
-            <div className="form-control">
-                <h2>Today's ToDo List 🌷</h2>
-                <input type="text" onChange={toDoInputHandler}/>
-            </div>
-            <button type="submit"><i class="icon ion-md-lock"></i> ADD</button>
-        </form>
-    );
+  return (
+    <form onSubmit={formSubmitHandler}>
+      <div className="form-control">
+        <h2>Today's ToDo List 🌷</h2>
+        <input type="text" onChange={toDoInputHandler} />
+      </div>
+      <button type="submit">
+        <i class="icon ion-md-lock"></i> ADD
+      </button>
+    </form>
+  );
 };
 
 export default ToDoInput;
