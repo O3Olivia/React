7076fb09d0158d3d38ee7c91d9779bb23cee6d09
O3Olivia <cherishtmwu@gmail.com>
2022-12-07 14:56:16 +0900

    add firebase auth api to login/signup form

--- third_react/simple-ecommerce/src/components/Layout/MainNavigation.js
+++ third_react/simple-ecommerce/src/components/Layout/MainNavigation.js
@@ -1,10 +1,17 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useContext } from "react";
 import { Link } from "react-router-dom";
-
+import LogContext from "../../store/log-context";
 import logo from "../../assets/logo2.png";
 import classes from "./MainNavigation.module.css";
 
 const MainNavigation = () => {
+  const logCtx = useContext(LogContext);
+  const isLoggedIn = logCtx.isLoggedIn;
+
+  const logoutHandler = () => {
+    alert("See you again👋");
+    logCtx.logout();
+  };
   return (
     <Fragment>
       <header className={classes.header}>
@@ -18,15 +25,20 @@ const MainNavigation = () => {
             <li>
               <Link to="/products">Products</Link>
             </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-            <li>
-              <Link to="/changePwd">change Password</Link>
-            </li>
-            <li>
-              <button>Logout</button>
-            </li>
+            {isLoggedIn && (
+              <li>
+                <Link to="/changePwd">change Password</Link>
+              </li>
+            )}
+            {isLoggedIn ? (
+              <li>
+                <button onClick={logoutHandler}>Logout</button>
+              </li>
+            ) : (
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+            )}
           </ul>
         </nav>
       </header>
--- third_react/simple-ecommerce/src/components/auth/Login.js
+++ third_react/simple-ecommerce/src/components/auth/Login.js
@@ -1,9 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import LogContext from "../../store/log-context";
 
 import classes from "./Login.module.css";
 import { RiEyeLine, RiEyeCloseLine } from "react-icons/ri";
+
 const Login = (props) => {
+  const navigate = useNavigate();
+
+  const emailInputRef = useRef();
+  const pwdInputRef = useRef();
+
+  const LogCtx = useContext(LogContext);
+
   const [isLogin, setIsLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+
   const [showPwd, setShowPwd] = useState({
     type: "password",
     visible: false,
@@ -22,18 +34,80 @@ const Login = (props) => {
     });
   };
 
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPwd = pwdInputRef.current.value;
+
+    setIsLogin(true);
+    let url;
+    if (isLogin) {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyA4bpMeHWiBx73UGNr9rw0xi0iysGp5feo";
+    } else {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyA4bpMeHWiBx73UGNr9rw0xi0iysGp5feo";
+    }
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        email: enteredEmail,
+        password: enteredPwd,
+        returnSecureToken: true,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        setIsLoading(false);
+        if (response.ok) {
+          return response.json();
+        } else {
+          return response.json().then((data) => {
+            let errorMsg = "Authentication Failed!";
+
+            throw new Error(errorMsg);
+          });
+        }
+      })
+      .then((data) => {
+        const expirationTime = new Date(
+          new Date().getTime() + +data.expiresIn * 1000
+        );
+        LogCtx.login(data.idToken, expirationTime.toISOString());
+
+        navigate("/", { replace: true });
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+    console.log("회원가입 성공!");
+  };
   return (
     <>
-      <form className={classes.loginForm}>
+      <form className={classes.loginForm} onSubmit={submitHandler}>
         <h1>{isLogin ? "로그인" : "회원가입"}</h1>
         {isLogin ? (
           ""
         ) : (
           <input id="name" type="text" placeholder="name" required />
         )}
-        <input id="email" type="email" placeholder="email" required />
+        <input
+          id="email"
+          type="email"
+          placeholder="email"
+          required
+          ref={emailInputRef}
+        />
         <div className={classes.passwordInput}>
-          <input id="pwd" placeholder="password" required type={showPwd.type} />
+          <input
+            id="pwd"
+            placeholder="password"
+            required
+            type={showPwd.type}
+            ref={pwdInputRef}
+          />
           <button className={classes.pwdShow} onClick={showPwdHandler}>
             {!showPwd.visible ? <RiEyeCloseLine /> : <RiEyeLine />}
           </button>
@@ -46,13 +120,13 @@ const Login = (props) => {
         </div>
         <div className={classes.signUp}>
           <span>
-            {isLogin
-              ? "어머! 이미 SK의 사랑스러운 회원이신가요?"
-              : "잠깐! 혹시 projectName이 처음이신가요?"}
+            {!isLogin
+              ? "어머! 이미 SK의 회원이신가요?"
+              : "잠깐! 혹시 SK가 처음이신가요?"}
           </span>
           <span className={classes.pullRight}>
             <button onClick={switchLoginHandler}>
-              {isLogin ? "로그인" : "회원가입"}
+              {!isLogin ? "로그인" : "회원가입"}
             </button>
           </span>
         </div>
