a6330e7ce429843087a4e94a7720c57742155342
O3Olivia <cherishtmwu@gmail.com>
2022-12-20 17:30:36 +0900

    add delete quote button

--- third_react/quotes/src/components/quotes/HighlightedQuote.js
+++ third_react/quotes/src/components/quotes/HighlightedQuote.js
@@ -1,8 +1,26 @@
 import classes from "./HighlightedQuote.module.css";
+import { useHistory } from "react-router-dom";
 
 const HighlightedQuote = (props) => {
+  const history = useHistory();
+  const deleteHandler = () => {
+    const url = `https://react-http-2a48b-default-rtdb.asia-southeast1.firebasedatabase.app/quotes/${props.id}.json`;
+    fetch(url, {
+      method: "DELETE",
+      header: {
+        "Content-Type": "application/json",
+      },
+    }).then((res) => {
+      if (!res.ok) {
+        throw new Error("Sth went wrong");
+      }
+      history.push("/quotes");
+    });
+    console.log(props.id);
+  };
   return (
     <figure className={classes.quote}>
+      <button onClick={deleteHandler}>DELETE</button>
       <p>{props.text}</p>
       <figcaption>{props.author}</figcaption>
       <p>{props.date}</p>
